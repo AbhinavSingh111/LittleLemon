a4d8e29d4ef0680a7f1a3aef0304c1958a1fb4f9
Abhinav Singh <singh111abhinav@gmail.com>
2023-08-02 00:41:16 +0530

    Added BookingViewSet,Installed django-filter,Used Default Router in Apps url for booking viewset,

--- reservation/views.py
+++ reservation/views.py
@@ -1,4 +1,30 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import generics,viewsets
+from rest_framework.response import Response
+from .models import Menu,Booking
+from .serializers import MenuSerializer,BookingSerializer
 
 def index(request):
     return render(request, 'index.html', {})
+
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data, many=isinstance(request.data, list))
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=201, headers=headers)
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView,generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
